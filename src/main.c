c5e8f3f2a4a56fafe252ce7a5d3770410188620a
mike765 <mike765>
2018-02-24 19:00:13 +0200

    FreeRTOS is porting.

--- src/main.c
+++ src/main.c
@@ -11,10 +11,23 @@
 
 #include "stm32f7xx.h"
 #include "stm32f769i_discovery.h"
+#include "FreeRTOS.h"
+#include "task.h"
 			
+void task1(void)
+{
+
+}
 
 int main(void)
 {
+	xTaskCreate( task1,				/* The function that implements the task. */
+							"task1", 								/* The text name assigned to the task - for debug only as it is not used by the kernel. */
+							2048, 			/* The size of the stack to allocate to the task. */
+							NULL, 								/* The parameter passed to the task - not used in this case. */
+							10, 	/* The priority assigned to the task. */
+							NULL );
+	vTaskStartScheduler();
 
 	for(;;);
 }
